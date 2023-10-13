849f71359ac162c8a89f52cb6c1345fe919d7977
lmartin2422 <lmartin2422@gmail.com>
2023-10-13 00:57:00 -0400

    completed the location dialog component

--- Interface/TruckingInterface/src/app/components/location-dialog/location-dialog.component.ts
+++ Interface/TruckingInterface/src/app/components/location-dialog/location-dialog.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-location-dialog',
-  templateUrl: './location-dialog.component.html',
-  styleUrls: ['./location-dialog.component.css']
+  // templateUrl: './location-dialog.component.html',
+  // styleUrls: ['./location-dialog.component.css']
+  template: `
+  <h2 mat-dialog-title>Add a new location</h2>
+  <mat-dialog-content>
+    <!-- Your form or input field goes here -->
+    <mat-form-field>
+      <input matInput placeholder="Address">
+      <input matInput placeholder="City">
+      <input matInput placeholder="State">
+      <input matInput placeholder="Zip Code">
+    </mat-form-field>
+  </mat-dialog-content>
+  <mat-dialog-actions>
+    <button mat-button (click)="onNoClick()">Cancel</button>
+    <button mat-button [mat-dialog-close]="data" cdkFocusInitial>Add</button>
+  </mat-dialog-actions>
+  `
 })
 export class LocationDialogComponent {
 
+  constructor(
+    public dialogRef: MatDialogRef<LocationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  
 }
